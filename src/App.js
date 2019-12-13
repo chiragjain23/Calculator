44604d9df70fe4d4b1eeb797536304f87c12c077
chiragjain23 <cjain780@yahoo.in>
2019-12-14 01:34:09 +0530

    Added Numeric Buttons  and View Screen to highlight number on button click.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
+function ViewScreen(props) {
+  return <div className="viewScreen">{props.value}</div>;
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      opStr: ""
+    };
+  }
+
+  generateOpString = value => {
+    this.setState(state => {
+      return { opStr: state.opStr + value };
+    });
+  };
+
+  render() {
+    return (
+      <div className="calculator">
+        <ViewScreen value={this.state.opStr} />
+
+        <div className="keys">
+          <input
+            type="button"
+            className="key"
+            value="1"
+            onClick={() => this.generateOpString(1)}
+          />
+          <input
+            type="button"
+            className="key"
+            value="2"
+            onClick={() => this.generateOpString(2)}
+          />
+          <input
+            type="button"
+            className="key"
+            value="3"
+            onClick={() => this.generateOpString(3)}
+          />
+        </div>
+        <div className="keys">
+          <input
+            type="button"
+            className="key"
+            value="4"
+            onClick={() => this.generateOpString(4)}
+          />
+          <input
+            type="button"
+            className="key"
+            value="5"
+            onClick={() => this.generateOpString(5)}
+          />
+          <input
+            type="button"
+            className="key"
+            value="6"
+            onClick={() => this.generateOpString(6)}
+          />
+        </div>
+        <div className="keys">
+          <input
+            type="button"
+            className="key"
+            value="7"
+            onClick={() => this.generateOpString(7)}
+          />
+          <input
+            type="button"
+            className="key"
+            value="8"
+            onClick={() => this.generateOpString(8)}
+          />
+          <input
+            type="button"
+            className="key"
+            value="9"
+            onClick={() => this.generateOpString(9)}
+          />
+        </div>
+        <div className="keys">
+          <input
+            type="button"
+            className="key"
+            value="0"
+            onClick={() => this.generateOpString(0)}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calculator />
     </div>
   );
 }
